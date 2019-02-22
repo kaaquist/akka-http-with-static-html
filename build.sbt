3499de1a16deae1114eec44975dcabf67dee017a
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2019-02-22 23:13:29 +0100

    Add - added css and js to Static route. #1 Css and JS added to project

--- build.sbt
+++ build.sbt
@@ -5,5 +5,3 @@ version := "1.0"
 scalaVersion := "2.12.1"
 
 libraryDependencies += "com.typesafe.akka" % "akka-http_2.12" % "10.0.4"
-
-    
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 
 /**
   * Created by kasper on 02/03/2017.
@@ -11,14 +12,25 @@ object Main {
 
     implicit val actorSystem = ActorSystem("system")
     implicit val actorMaterializer = ActorMaterializer()
-
-
-    val route =
-      pathSingleSlash {
-        get {
-          getFromResource("index.html")
-        }
+    def index = path("") {
+      getFromResource("index.html")
+    }
+    def css = (pathPrefix("css") & path(Segment)) { resource => {
+      println("what!")
+      getFromResource(s"css/$resource")
       }
+    }
+    def js = (pathPrefix("js") & path(Segment)) { resource => getFromResource(s"js/$resource") }
+
+    lazy val route: Route =
+      pathEndOrSingleSlash {
+        getFromResource("index.html")
+      } ~ path("css" / Segment) { name =>
+        getFromResource("css/" + name)
+      } ~ path("js" / Segment) { name =>
+        getFromResource("js/" + name)
+      }
+
     Http().bindAndHandle(route,"localhost",8080)
 
     println("server started at 8080")
